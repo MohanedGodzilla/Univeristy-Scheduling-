f8a4eefc934d26a10c9f79106c488fb187af3d3f
Mohanad Godzilla <39066284+MohanedGodzilla@users.noreply.github.com>
2020-04-11 21:52:17 +0200

    adding course view without dataconnection

--- university_scheduler/Form1.Designer.cs
+++ university_scheduler/Form1.Designer.cs
@@ -29,33 +29,71 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoScheduleHome));
+            this.coursesView = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.programsView = new System.Windows.Forms.TabPage();
+            this.classroomsView = new System.Windows.Forms.TabPage();
+            this.resourcesView = new System.Windows.Forms.TabPage();
             this.panel2 = new System.Windows.Forms.Panel();
             this.etInput = new System.Windows.Forms.DateTimePicker();
             this.stInput = new System.Windows.Forms.DateTimePicker();
             this.etLabel = new System.Windows.Forms.Label();
             this.stLabel = new System.Windows.Forms.Label();
             this.generateBTN = new System.Windows.Forms.Button();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.addCourseBTN = new System.Windows.Forms.Button();
-            this.addClassroomBTN = new System.Windows.Forms.Button();
-            this.addProgramBTN = new System.Windows.Forms.Button();
-            this.addResourceBTN = new System.Windows.Forms.Button();
-            this.coursesLabel = new System.Windows.Forms.Label();
-            this.classroomsLabel = new System.Windows.Forms.Label();
-            this.programsLabel = new System.Windows.Forms.Label();
-            this.resourcesLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // coursesView
+            // 
+            resources.ApplyResources(this.coursesView, "coursesView");
+            this.coursesView.Name = "coursesView";
+            this.coursesView.UseVisualStyleBackColor = true;
             // 
             // tableLayoutPanel1
             // 
             resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
-            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.coursesView);
+            this.tabControl1.Controls.Add(this.programsView);
+            this.tabControl1.Controls.Add(this.classroomsView);
+            this.tabControl1.Controls.Add(this.resourcesView);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // programsView
+            // 
+            resources.ApplyResources(this.programsView, "programsView");
+            this.programsView.Name = "programsView";
+            this.programsView.UseVisualStyleBackColor = true;
+            // 
+            // classroomsView
+            // 
+            resources.ApplyResources(this.classroomsView, "classroomsView");
+            this.classroomsView.Name = "classroomsView";
+            this.classroomsView.UseVisualStyleBackColor = true;
+            // 
+            // resourcesView
+            // 
+            resources.ApplyResources(this.resourcesView, "resourcesView");
+            this.resourcesView.Name = "resourcesView";
+            this.resourcesView.UseVisualStyleBackColor = true;
             // 
             // panel2
             // 
@@ -66,7 +104,6 @@
             this.panel2.Controls.Add(this.generateBTN);
             resources.ApplyResources(this.panel2, "panel2");
             this.panel2.Name = "panel2";
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // etInput
             // 
@@ -97,74 +134,6 @@
             this.generateBTN.ForeColor = System.Drawing.Color.Black;
             this.generateBTN.Name = "generateBTN";
             this.generateBTN.UseVisualStyleBackColor = false;
-            this.generateBTN.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // tableLayoutPanel2
-            // 
-            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
-            this.tableLayoutPanel2.Controls.Add(this.addCourseBTN, 0, 2);
-            this.tableLayoutPanel2.Controls.Add(this.addClassroomBTN, 1, 2);
-            this.tableLayoutPanel2.Controls.Add(this.addProgramBTN, 2, 2);
-            this.tableLayoutPanel2.Controls.Add(this.addResourceBTN, 3, 2);
-            this.tableLayoutPanel2.Controls.Add(this.coursesLabel, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.classroomsLabel, 1, 0);
-            this.tableLayoutPanel2.Controls.Add(this.programsLabel, 2, 0);
-            this.tableLayoutPanel2.Controls.Add(this.resourcesLabel, 3, 0);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
-            // 
-            // addCourseBTN
-            // 
-            this.addCourseBTN.BackColor = System.Drawing.SystemColors.Highlight;
-            this.addCourseBTN.DialogResult = System.Windows.Forms.DialogResult.OK;
-            resources.ApplyResources(this.addCourseBTN, "addCourseBTN");
-            this.addCourseBTN.Name = "addCourseBTN";
-            this.addCourseBTN.UseVisualStyleBackColor = false;
-            this.addCourseBTN.Click += new System.EventHandler(this.addCourseBTN_Click);
-            // 
-            // addClassroomBTN
-            // 
-            this.addClassroomBTN.BackColor = System.Drawing.SystemColors.Highlight;
-            resources.ApplyResources(this.addClassroomBTN, "addClassroomBTN");
-            this.addClassroomBTN.Name = "addClassroomBTN";
-            this.addClassroomBTN.UseVisualStyleBackColor = false;
-            this.addClassroomBTN.Click += new System.EventHandler(this.addClassroomBTN_Click);
-            // 
-            // addProgramBTN
-            // 
-            this.addProgramBTN.BackColor = System.Drawing.SystemColors.Highlight;
-            resources.ApplyResources(this.addProgramBTN, "addProgramBTN");
-            this.addProgramBTN.Name = "addProgramBTN";
-            this.addProgramBTN.UseVisualStyleBackColor = false;
-            this.addProgramBTN.Click += new System.EventHandler(this.addProgramBTN_Click);
-            // 
-            // addResourceBTN
-            // 
-            this.addResourceBTN.BackColor = System.Drawing.SystemColors.Highlight;
-            resources.ApplyResources(this.addResourceBTN, "addResourceBTN");
-            this.addResourceBTN.Name = "addResourceBTN";
-            this.addResourceBTN.UseVisualStyleBackColor = false;
-            // 
-            // coursesLabel
-            // 
-            resources.ApplyResources(this.coursesLabel, "coursesLabel");
-            this.coursesLabel.Name = "coursesLabel";
-            // 
-            // classroomsLabel
-            // 
-            resources.ApplyResources(this.classroomsLabel, "classroomsLabel");
-            this.classroomsLabel.Name = "classroomsLabel";
-            // 
-            // programsLabel
-            // 
-            resources.ApplyResources(this.programsLabel, "programsLabel");
-            this.programsLabel.Name = "programsLabel";
-            // 
-            // resourcesLabel
-            // 
-            resources.ApplyResources(this.resourcesLabel, "resourcesLabel");
-            this.resourcesLabel.Name = "resourcesLabel";
-            this.resourcesLabel.Click += new System.EventHandler(this.label4_Click);
             // 
             // NoScheduleHome
             // 
@@ -174,32 +143,29 @@
             this.Name = "NoScheduleHome";
             this.Load += new System.EventHandler(this.NoScheduleHome_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TabPage coursesView;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage programsView;
+        private System.Windows.Forms.TabPage classroomsView;
+        private System.Windows.Forms.TabPage resourcesView;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DateTimePicker etInput;
         private System.Windows.Forms.DateTimePicker stInput;
         private System.Windows.Forms.Label etLabel;
         private System.Windows.Forms.Label stLabel;
         private System.Windows.Forms.Button generateBTN;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Button addCourseBTN;
-        private System.Windows.Forms.Button addClassroomBTN;
-        private System.Windows.Forms.Button addProgramBTN;
-        private System.Windows.Forms.Button addResourceBTN;
-        private System.Windows.Forms.Label coursesLabel;
-        private System.Windows.Forms.Label classroomsLabel;
-        private System.Windows.Forms.Label programsLabel;
-        private System.Windows.Forms.Label resourcesLabel;
     }
 }
 
--- university_scheduler/Form1.cs
+++ university_scheduler/Form1.cs
@@ -15,71 +15,19 @@ namespace university_scheduler
         public NoScheduleHome()
         {
             InitializeComponent();
+            courses_view();
         }
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void addCourseBTN_Click(object sender, EventArgs e)
-        {
-            addCourseForm addCoursePopup = new addCourseForm();
-            DialogResult dialogResult = addCoursePopup.ShowDialog();
-        }
-
-        private void addClassroomBTN_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void addProgramBTN_Click(object sender, EventArgs e)
-        {
-
-        }
-
+        
         private void NoScheduleHome_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void courses_view() {
+            viewCoursesForm frm = new viewCoursesForm() { Dock = DockStyle.Fill, TopLevel = false, TopMost = true };
+            this.coursesView.Controls.Add(frm);
+            frm.Show();
+        }
+
     }
 }
