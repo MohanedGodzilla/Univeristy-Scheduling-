35330a1d5d1cd71a738b63f16a9a19c3188f4529
Mohanad Godzilla <39066284+MohanedGodzilla@users.noreply.github.com>
2020-05-04 16:21:15 +0200

    update gitignore

--- university_scheduler/env.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace university_scheduler {
-    class env {
-        static public String db_con_str = "Data Source=OMARLAP\\SQLEXPRESS;Initial Catalog=course_scheduler;Integrated Security=True";
-    }
-}
