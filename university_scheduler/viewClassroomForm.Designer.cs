23b92f5df8776371b33a060349dc489054119cf7
Asim Issam Farhat <34529102+ASIM-ISSAM@users.noreply.github.com>
2020-08-06 16:27:52 +0200

    1

--- university_scheduler/viewClassroomForm.Designer.cs
+++ university_scheduler/viewClassroomForm.Designer.cs
@@ -28,30 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.classData = new System.Windows.Forms.DataGridView();
             this.newClassRoomBTN = new System.Windows.Forms.Button();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.deleteClassRoomBTN = new System.Windows.Forms.Button();
             this.editClassRoomBTN = new System.Windows.Forms.Button();
             this.deleteAllBTN = new System.Windows.Forms.Button();
+            this.classData = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.classData)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.classData)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.classData);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.classData);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
@@ -76,36 +74,6 @@
             this.label1.Size = new System.Drawing.Size(55, 18);
             this.label1.TabIndex = 3;
             this.label1.Text = "Search";
-            // 
-            // classData
-            // 
-            this.classData.AllowUserToAddRows = false;
-            this.classData.AllowUserToDeleteRows = false;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.classData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.classData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.classData.DefaultCellStyle = dataGridViewCellStyle2;
-            this.classData.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.classData.Location = new System.Drawing.Point(0, 62);
-            this.classData.Name = "classData";
-            this.classData.ReadOnly = true;
-            this.classData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.classData.Size = new System.Drawing.Size(611, 382);
-            this.classData.TabIndex = 5;
-            this.classData.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.classData_CellDoubleClick);
             // 
             // newClassRoomBTN
             // 
@@ -198,6 +166,21 @@
             this.deleteAllBTN.UseVisualStyleBackColor = false;
             this.deleteAllBTN.Click += new System.EventHandler(this.deleteAllBTN_Click);
             // 
+            // classData
+            // 
+            this.classData.AllowUserToAddRows = false;
+            this.classData.AllowUserToDeleteRows = false;
+            this.classData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.classData.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.classData.Location = new System.Drawing.Point(0, 79);
+            this.classData.Name = "classData";
+            this.classData.ReadOnly = true;
+            this.classData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.classData.Size = new System.Drawing.Size(611, 365);
+            this.classData.TabIndex = 5;
+            this.classData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.classData_CellContentClick);
+            this.classData.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.classData_CellDoubleClick);
+            // 
             // viewClassroomForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -211,9 +194,9 @@
             this.Load += new System.EventHandler(this.viewClassroomForm_Load_1);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.classData)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.classData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -225,10 +208,10 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.Button deleteClassRoomBTN;
         private System.Windows.Forms.Button editClassRoomBTN;
-        public System.Windows.Forms.DataGridView classData;
         public System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.Button deleteAllBTN;
+        public System.Windows.Forms.DataGridView classData;
     }
 }
